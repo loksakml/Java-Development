1b1e6cf84d330574aac6d89768ec2c29189b0162
Ka Man Lok <loksakml@gmail.com>
2017-12-14 14:27:14 +0000

    Scope Challenge Solution added

---
+++ ScopeChallenge/src/com/loska/Main.java
@@ -0,0 +1,16 @@
+package com.loska;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter an integer:");
+
+        X x = new X(scanner.nextDouble());
+        x.x();
+
+
+    }
+}
---
+++ ScopeChallenge/src/com/loska/X.java
@@ -0,0 +1,18 @@
+package com.loska;
+
+
+public class X {
+    private double x;
+
+    public X(double x) {
+        this.x = x;
+    }
+
+    public void x(){
+        for (int x = 0; x <=12 ; x++) {
+            System.out.println(this.x*x);
+        }
+    }
+}
+
+
