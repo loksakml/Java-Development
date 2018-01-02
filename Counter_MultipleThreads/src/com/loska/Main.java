77f1a119179bbb08da873fc9aac8b52976a96ab3
Ka Man Lok <loksakml@gmail.com>
2018-01-02 15:46:32 +0000

    Synchronized methods to avoid interference

---
+++ Counter_MultipleThreads/src/com/loska/Main.java
@@ -0,0 +1,64 @@
+package com.loska;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Countdown countdown = new Countdown();
+        CountdownThread t1 = new CountdownThread(countdown);
+        t1.setName("Thread 1");
+        CountdownThread t2 = new CountdownThread(countdown);
+        t2.setName("Thread 2");
+//        CountdownThread t3 = new CountdownThread(countdown);
+//        t1.setName("Thread 3");
+//        CountdownThread t4 = new CountdownThread(countdown);
+//        t1.setName("Thread 4");
+//        CountdownThread t5 = new CountdownThread(countdown);
+//        t1.setName("Thread 5");
+        t1.start();
+        t2.start();
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+class Countdown {
+    private int i;
+
+    public synchronized void doCountdown(){
+        String colour;
+        switch(Thread.currentThread().getName()){
+            case "Thread 1":
+                colour = ThreadColour.ANSI_CYAN;
+                break;
+            case "Thread 2":
+                colour = ThreadColour.ANSI_PURPLE;
+                break;
+            default:
+                colour = ThreadColour.ANSI_GREEN;
+        }
+
+        for (i = 10; i>0; i--){
+            System.out.println(colour + Thread.currentThread().getName() + ": i =" + i);
+        }
+    }
+}
+
+class CountdownThread extends Thread {
+    private Countdown threadCountdown;
+
+    public CountdownThread(Countdown countdown) {
+        threadCountdown = countdown;
+    }
+
+    public void run() {
+        threadCountdown.doCountdown();
+    }
+}
