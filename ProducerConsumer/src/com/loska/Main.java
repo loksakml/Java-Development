99fefa05c88321b064ac7a1a557ac834739df07e
Ka Man Lok <loksakml@gmail.com>
2018-01-03 16:14:04 +0000

    return value from a thread using executorService.submit into a Future class

--- ProducerConsumer/src/com/loska/Main.java
+++ ProducerConsumer/src/com/loska/Main.java
@@ -3,6 +3,7 @@ package com.loska;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static com.loska.Main.EOF;
@@ -13,14 +14,34 @@ public class Main {
     public static void main(String[] args) {
         List<String> buffer = new ArrayList<String>();
         ReentrantLock bufferLock = new ReentrantLock();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(5);//first you need a new threadPool
         MyProducer producer = new MyProducer(buffer, ThreadColour.ANSI_GREEN, bufferLock);
         MyConsumer consumer1 = new MyConsumer(buffer, ThreadColour.ANSI_PURPLE, bufferLock);
         MyConsumer consumer2 = new MyConsumer(buffer, ThreadColour.ANSI_CYAN, bufferLock);
 
-        new Thread(producer).start();
-        new Thread(consumer1).start();
-        new Thread(consumer2).start();
+        executorService.execute(producer);
+        executorService.execute(consumer1);
+        executorService.execute(consumer2);
 
+        Future<String> future = executorService.submit(new Callable<String>(){
+            @Override
+            public String call() throws Exception {
+                System.out.println(ThreadColour.ANSI_WHITE + "I'm being printed for the Callable class");
+                return "This is the callable result.";
+            }
+        });
+        try{
+            System.out.println(future.get());
+
+        } catch(ExecutionException e){
+            System.out.println("Something went wrong");
+        } catch(InterruptedException e){
+            System.out.println("Thread running the tast was interrupted");
+
+        }
+
+        executorService.shutdown();
 
     }
 }
