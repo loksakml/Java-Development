c6a92610eb06065e920cf7a713afd391cfb473c8
Ka Man Lok <loksakml@gmail.com>
2018-01-04 20:01:28 +0000

    throw your own exceptions

--- Exception/src/com/loska/Example.java
+++ Exception/src/com/loska/Example.java
@@ -1,5 +1,7 @@
 package com.loska;
 
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Example {
@@ -9,15 +11,33 @@ public class Example {
     }
 
     private static int divide(){
-        int x = getInt();
-        int y = getInt();
-        System.out.println("x is " + x +", y is " + y);
-        return x / y;
+        int x, y;
+        try {
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x +", y is " + y);
+            return x / y;
+        } catch (NoSuchElementException e){
+            throw new ArithmeticException( "no suitable input");
+        } catch (ArithmeticException e) {
+            throw new ArithmeticException("attempt to divide by zero");
+        }
+
     }
+
 
     private static int getInt() {
         Scanner s = new Scanner(System.in);
         System.out.println("Please enter an integer ");
-        return s.nextInt();
+        while (true) {
+            try{
+                return s.nextInt();
+            } catch (InputMismatchException e){
+                //go around again, Read pass the end of lin ein the input first.
+                s.nextLine();
+                System.out.println("Please enter a number using only the digits 0 to 9");
+            }
+        }
+
     }
 }
